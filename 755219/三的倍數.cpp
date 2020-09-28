93806e215327caec46bfe554cee5b276bb0c0c83
bradychen950410 <46493214+bradychen950410@users.noreply.github.com>
2020-09-28 10:11:02 +0800

    Update 三的倍數.cpp

--- 755219/三的倍數.cpp
+++ 755219/三的倍數.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-  int num;
-  cout << "請輸入數字：\n";
-  cin >> num;
-  if (num % 3 == 0) {
-    cout << "This is 三的倍數";
-  } else {
-    cout << "This is not 三的倍數";
-  }
+    int num;
+    cout << "請輸入數字：\n";
+    cin >> num;
+    if (num % 3 == 0) {
+      cout << "This is 三的倍數";
+    } else {
+      cout << "This is not 三的倍數";
+    }
 }
